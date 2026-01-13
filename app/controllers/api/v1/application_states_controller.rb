8798a8e4f1151474fc3525675648da9e0a82aa4b
Christopher Johnston <chris@fuzzylizard.com>
2026-01-13 14:33:09 -0500

    Small rubocop changes, removes packages from Rails app

--- app/controllers/api/v1/application_states_controller.rb
+++ app/controllers/api/v1/application_states_controller.rb
@@ -1,55 +1,60 @@
 # frozen_string_literal: true
 
-class Api::V1::ApplicationStatesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+module Api
+  module V1
+    # Understands handling API requests for ApplicationState resources
+    class ApplicationStatesController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
-  # GETS /api/v1/application_states
-  def index
-    application_states = ApplicationState.all.order(:id)
+      # GETS /api/v1/application_states
+      def index
+        application_states = ApplicationState.all.order(:id)
 
-    render json: application_states
-  end
+        render json: application_states
+      end
 
-  # GET /api/v1/application_states/:id
-  def show
-    application_state = ApplicationState.find(params[:id])
+      # GET /api/v1/application_states/:id
+      def show
+        application_state = ApplicationState.find(params[:id])
 
-    render json: application_state
-  end
+        render json: application_state
+      end
 
-  # POST /api/v1/application_states
-  def create
-    application_state = ApplicationState.new(application_state_params)
+      # POST /api/v1/application_states
+      def create
+        application_state = ApplicationState.new(application_state_params)
 
-    if application_state.save
-      render json: application_state, status: :created
-    else
-      render json: application_state.errors, status: :unprocessable_entity
+        if application_state.save
+          render json: application_state, status: :created
+        else
+          render json: application_state.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PUT /api/v1/application_states/:id
+      def update
+        application_state = ApplicationState.find(params[:id])
+
+        if application_state.update(application_state_params)
+          render json: application_state
+        else
+          render json: application_state.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /api/v1/application_states/:id
+      def destroy
+        application_state = ApplicationState.find(params[:id])
+        application_state.destroy
+
+        head :no_content
+      end
+
+      private
+
+      def application_state_params
+        params.require(:application_state).permit(:name)
+      end
     end
-  end
-
-  # PUT /api/v1/application_states/:id
-  def update
-    application_state = ApplicationState.find(params[:id])
-
-    if application_state.update(application_state_params)
-      render json: application_state
-    else
-      render json: application_state.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /api/v1/application_states/:id
-  def destroy
-    application_state = ApplicationState.find(params[:id])
-    application_state.destroy
-
-    head :no_content
-  end
-
-  private
-
-  def application_state_params
-    params.require(:application_state).permit(:name)
   end
 end
--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,61 +1,66 @@
 # frozen_string_literal: true
 
-class Api::V1::JobsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+module Api
+  module V1
+    # Understands job-related API actions
+    class JobsController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
-  # Returns all jobs
-  # GET /api/v1/jobs
-  def index
-    if params[:application_state_id]
-      jobs = Job.where(application_state_id: params[:application_state_id])
-    else
-      jobs = Job.all
+      # Returns all jobs
+      # GET /api/v1/jobs
+      def index
+        jobs = if params[:application_state_id]
+                 Job.where(application_state_id: params[:application_state_id])
+               else
+                 Job.all
+               end
+        render json: jobs
+      end
+
+      # Gets a specific job by ID
+      # GET /api/v1/jobs/:id
+      def show
+        job = Job.find(params[:id])
+        render json: job
+      end
+
+      # Creates a new job
+      # POST /api/v1/jobs
+      def create
+        job = Job.new(job_params)
+
+        if job.save
+          render json: job, status: :created
+        else
+          render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # Updates a job
+      # PUT /api/v1/jobs/:id
+      def update
+        job = Job.find(params[:id])
+
+        if job.update(job_params)
+          render json: job
+        else
+          render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # Deletes a job
+      # DELETE /api/v1/jobs/:id
+      def destroy
+        job = Job.find(params[:id])
+        job.destroy
+        head :no_content
+      end
+
+      private
+
+      def job_params
+        params.require(:job).permit(:company_name, :job_ad_url, :job_title, :company_url, :notes, :application_state_id)
+      end
     end
-    render json: jobs
-  end
-
-  # Gets a specific job by ID
-  # GET /api/v1/jobs/:id
-  def show
-    job = Job.find(params[:id])
-    render json: job
-  end
-
-  # Creates a new job
-  # POST /api/v1/jobs
-  def create
-    job = Job.new(job_params)
-
-    if job.save
-      render json: job, status: :created
-    else
-      render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  # Updates a job
-  # PUT /api/v1/jobs/:id
-  def update
-    job = Job.find(params[:id])
-
-    if job.update(job_params)
-      render json: job
-    else
-      render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  # Deletes a job
-  # DELETE /api/v1/jobs/:id
-  def destroy
-    job = Job.find(params[:id])
-    job.destroy
-    head :no_content
-  end
-
-  private
-
-  def job_params
-    params.require(:job).permit(:company_name, :job_ad_url, :job_title, :company_url, :notes, :application_state_id)
   end
 end
